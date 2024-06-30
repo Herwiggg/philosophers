ea3fd7676d8b1775056dd7566dcb2f9d1828666f
Herwiggg <michelot.alexy78@gmail.com>
2024-06-30 03:33:22 +0200

    working, need to implement dead now

--- philo/dinner.c
+++ philo/dinner.c
@@ -6,7 +6,7 @@
 /*   By: almichel <almichel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 17:23:47 by almichel          #+#    #+#             */
-/*   Updated: 2024/06/29 19:16:38 by almichel         ###   ########.fr       */
+/*   Updated: 2024/06/30 03:01:45 by almichel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,10 +48,10 @@ void	dinner_start(t_table *table)
 		return;
 	else
 	{
+		table->start_time = gettime(MILLISECOND);
 		while (++i < table->num_of_philos)
 			pthread_create(&table->philos[i].thread_id, NULL, dinner_simulation, &table->philos[i]);
 	}
-	table->start_time = gettime(MILLISECOND);
 	set_bool(&table->table_mutex, &table->thread_ready, true);
 	i = -1;
 	while (++i < table->num_of_philos)
@@ -63,7 +63,9 @@ void	*dinner_simulation(void *data)
 	t_philo *philo;
 
 	philo = (t_philo *)data;
-	wait_threads(philo->table);
+//	wait_threads(philo->table);
+	if (philo->id % 2 != 0)
+		precise_usleep(philo->table->time_to_eat, philo->table);
 	while (!simulation_finished(philo->table))
 	{
 		if (philo->full)
